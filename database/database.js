a6404e312f47aa776c0cea70911b454fd40e283c
Diego <ddzadravec@gmail.com>
2020-09-20 02:03:40 -0300

    ajuste para mobile

--- database/database.js
+++ database/database.js
@@ -4,8 +4,11 @@ const connection = new Sequelize(
   "questions_answer",
   "diegozf",
   "?E8[H2UalZ+mV",
+  //"root",
+  //"",
   {
     host: "mysql669.umbler.com",
+    //host: "localhost",
     dialect: "mysql",
   }
 );
