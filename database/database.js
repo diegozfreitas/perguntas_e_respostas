530accdb3ae81113fbd471ef16e323d7ba4d130c
Diego <ddzadravec@gmail.com>
2020-09-19 21:36:28 -0300

    definindo bando de dados online

--- database/database.js
+++ database/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("questions_answers", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const connection = new Sequelize(
+  "questions_answers",
+  "diegozf",
+  "Ln,}l49xW_Q",
+  {
+    host: "mysql669.umbler.com",
+    dialect: "mysql",
+  }
+);
 
 module.exports = connection;
